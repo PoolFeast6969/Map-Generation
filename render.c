9f89ec5209ba00c172a4dd6c8c5897ed3070942c
RedBeardCory <corypatto@gmail.com>
2017-11-06 21:37:15 +1100

    added makefile

--- render.c
+++ render.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#include <SDL2/SDL.h>
+#include "SDL2/SDL.h"
 
 int render(long double height[], int size){
     //Start SDL
     SDL_Init( SDL_INIT_VIDEO );
-    
+
     SDL_Window *window = SDL_CreateWindow(
         "get ",
         SDL_WINDOWPOS_UNDEFINED,
@@ -25,14 +25,14 @@ int render(long double height[], int size){
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0");
     //SDL_RenderSetLogicalSize(renderer, size, size);
     //SDL_RenderSetScale(renderer, 4,4);
-    
+
     SDL_Texture *land_texture = SDL_CreateTexture(
         renderer,
         SDL_PIXELFORMAT_RGB332,
         SDL_TEXTUREACCESS_STATIC,
         size, size
     );
-    
+
     SDL_Texture *cloud_shadow_texture = SDL_CreateTexture(
         renderer,
         SDL_PIXELFORMAT_RGB332,
@@ -40,16 +40,16 @@ int render(long double height[], int size){
         size, size
     );
 
-    
+
     SDL_Event event;
     int width;
-    
+
     // Convert height map to pixel color map
     Uint32 format;
     SDL_PixelFormat *fmt;
     SDL_QueryTexture(land_texture, &format, NULL, NULL, &width);
     fmt = SDL_AllocFormat(format);
-    
+
     Uint8 land_pixels[size*size];
     for(int columns=0; columns < size*size; columns++) {
         if (height[columns] == 0) {
@@ -70,10 +70,10 @@ int render(long double height[], int size){
             }
         }
     }
-    
+
     // Put the background into a texture
     SDL_UpdateTexture(land_texture, NULL, land_pixels, width * sizeof(Uint8));
-    
+
     // Create some clouds
     Uint8 cloud_pixels[size*size];
     for(int columns=0; columns < size*size; columns++) {
@@ -91,16 +91,16 @@ int render(long double height[], int size){
     SDL_SetColorKey(cloud_surface, SDL_TRUE, SDL_MapRGB(fmt, 0, 0, 0));
     SDL_Texture *cloud_texture = SDL_CreateTextureFromSurface(renderer,cloud_surface);
     SDL_FreeSurface( cloud_surface );
-    
+
     // Something
     SDL_FreeFormat(fmt);
 
     SDL_Rect land_source = {0,0,size,size};
     SDL_Rect land_dest = {0,0,size,size};
-    
+
     SDL_Rect cloud_source = {0,0,size,size};
     SDL_Rect cloud_dest = {0,0,size,size};
-    
+
     // Main loop that updates at vsync in case we ever need animations
     while (true) {
         SDL_PollEvent(&event);
@@ -109,14 +109,14 @@ int render(long double height[], int size){
         }
         // Stop the thing from stretching the background
         //SDL_GetRendererOutputSize(renderer, &land_source.w, &land_source.h);
-        
+
         // Add the land to the frame
         SDL_RenderCopy(renderer,land_texture,&land_source,&land_dest);
         // Add the clouds to the frame
         SDL_RenderCopyEx(renderer, cloud_texture, &cloud_source, &cloud_dest, 90, NULL, SDL_FLIP_VERTICAL);
         // Show the completed frame and wait for vsync
         SDL_RenderPresent(renderer);
-        
+
         // Move clouds really badly
         cloud_dest.x++;
     }
@@ -128,4 +128,3 @@ int render(long double height[], int size){
     SDL_Quit();
     return 0;
 }
-
