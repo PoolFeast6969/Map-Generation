915ea200eee08dea5d320444ff4747a73b4800b1
Jack Williams <jack@Mac-mini.local>
2017-11-07 19:31:06 +1100

    wow

--- render.c
+++ render.c
@@ -20,24 +20,19 @@ int render(long double height[], int size){
         window,
         -1,
         SDL_RENDERER_ACCELERATED |
-        SDL_RENDERER_PRESENTVSYNC
+        SDL_RENDERER_PRESENTVSYNC |
+        SDL_RENDERER_TARGETTEXTURE
     );
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0");
     //SDL_RenderSetLogicalSize(renderer, size, size);
     //SDL_RenderSetScale(renderer, 4,4);
-    
-    SDL_Texture *land_texture = SDL_CreateTexture(
-        renderer,
-        SDL_PIXELFORMAT_RGB332,
-        SDL_TEXTUREACCESS_STATIC,
-        size, size
-    );
     
     SDL_Event event;
     int width;
     
     Uint32 format;
     SDL_PixelFormat *fmt;
+    SDL_Texture *land_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB332, SDL_TEXTUREACCESS_STATIC, size, size);
     SDL_QueryTexture(land_texture, &format, NULL, NULL, &width);
     fmt = SDL_AllocFormat(format);
     
@@ -54,7 +49,7 @@ int render(long double height[], int size){
                 land_pixels[columns] = SDL_MapRGB(fmt, 255, 255, 150);
             } else if (height[columns] < 6) {
                 // Forest
-                int greenness = (height[columns]/6) * 130 + 90;
+                int greenness = height[columns]/6 * 130 + 90;
 
                 land_pixels[columns] = SDL_MapRGB(fmt, 0, greenness, 0);
             } else if (height[columns] >= 6) {
@@ -71,7 +66,6 @@ int render(long double height[], int size){
     
     // Create some clouds
     Uint8 cloud_pixels[size*size];
-    
     for(int columns=0; columns < size*size; columns++) {
         if (height[columns] < 3) {
             // Transparent
@@ -81,29 +75,39 @@ int render(long double height[], int size){
             int greyness = (1-(height[columns]-3)/16) * 225 + 30;
             cloud_pixels[columns] = SDL_MapRGB(fmt, greyness, greyness, greyness);
         }
-        //printf("%c ",cloud_pixels[columns]);
     }
+    SDL_Texture *cloud_complete_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB332, SDL_TEXTUREACCESS_TARGET, size, size);
+    SDL_SetRenderTarget(renderer, cloud_complete_texture);
     SDL_Surface *cloud_surface = SDL_CreateRGBSurfaceWithFormatFrom(cloud_pixels, size, size, 0,width * sizeof(Uint8), format);
     SDL_SetColorKey(cloud_surface, SDL_TRUE, SDL_MapRGB(fmt, 0, 0, 0));
     SDL_Texture *cloud_texture = SDL_CreateTextureFromSurface(renderer,cloud_surface);
     SDL_Rect cloud_source = {0,0,size,size};
     SDL_Rect cloud_dest = {0,0,size,size};
-    
     // Create cloud shadows by blacking out the cloud texture
     SDL_Texture *cloud_shadow_texture = SDL_CreateTextureFromSurface(renderer,cloud_surface);
     SDL_SetTextureColorMod(cloud_shadow_texture, 30, 30, 30);
-    SDL_SetTextureAlphaMod(cloud_shadow_texture, 140); // Find a way of setting alpha blend for texture rather than for each pixel
-    SDL_FreeSurface( cloud_surface );
+    SDL_SetTextureAlphaMod(cloud_shadow_texture, 140);
     SDL_Rect cloud_shadow_source = {0,0,size,size};
     SDL_Rect cloud_shadow_dest = {-15,25,size,size};
+    // Add the cloud shadows to the frame
+    SDL_SetTextureBlendMode(cloud_complete_texture, SDL_BLENDMODE_BLEND);
+    SDL_RenderCopyEx(renderer, cloud_shadow_texture, &cloud_shadow_source, &cloud_shadow_dest, 90, NULL, SDL_FLIP_NONE);
+    // Add the clouds to the frame
+    SDL_RenderCopyEx(renderer, cloud_texture, &cloud_source, &cloud_dest, 90, NULL, SDL_FLIP_NONE);
+    
     
     // Something
-    SDL_FreeFormat(fmt);
+    SDL_SetRenderTarget(renderer, NULL);
     
     int window_h;
     int window_w;
     
+    cloud_dest.x = -200;
+    
+    float cloud_speed = 0.01; // In pixels per millisecond
+    
     // Main loop that updates at vsync in case we ever need animations
+    Uint32 last_update_time = SDL_GetTicks();
     while (true) {
         SDL_PollEvent(&event);
         if (event.type == SDL_QUIT) {
@@ -112,23 +116,24 @@ int render(long double height[], int size){
         // Get the window size
         SDL_GetRendererOutputSize(renderer, &window_w, &window_h);
         
-        land_dest.w = window_w*4;
-        land_dest.h = window_w*4;
+        land_dest.w = window_w*3;
+        land_dest.h = window_w*3;
         
-        cloud_shadow_dest.w = window_w*4;
-        cloud_shadow_dest.h = window_w*4;
+        cloud_dest.w = window_w*3;
+        cloud_dest.h = window_w*3;
         
-        cloud_dest.w = window_w*4;
-        cloud_dest.h = window_w*4;
+        land_source.w = window_w*3;
         
-        land_source.w = window_w*4;
-
+        Uint32 time_since_update = SDL_GetTicks() - last_update_time;
+        if ((int)(time_since_update * cloud_speed) != 0) {
+            cloud_dest.x = cloud_dest.x + (int)(time_since_update * cloud_speed);
+            last_update_time = SDL_GetTicks();
+        }
+        
         // Add the land to the frame
         SDL_RenderCopy(renderer,land_texture,&land_source,&land_dest);
-        // Add the cloud shadows to the frame
-        SDL_RenderCopyEx(renderer, cloud_shadow_texture, &cloud_shadow_source, &cloud_shadow_dest, 90, NULL, SDL_FLIP_NONE);
-        // Add the clouds to the frame
-        SDL_RenderCopyEx(renderer, cloud_texture, &cloud_source, &cloud_dest, 90, NULL, SDL_FLIP_NONE);
+        // Add the clouds
+        SDL_RenderCopy(renderer,cloud_complete_texture, &cloud_source, &cloud_dest);
         // Show the completed frame and wait for vsync
         SDL_RenderPresent(renderer);
         
