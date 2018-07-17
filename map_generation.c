d937e5ac1c79ea05d1a350543f6735f92b3c89de
Conman2 <richardharris443@gmail.com>
2018-07-17 20:29:43 +1000

    fuheffeu

--- map_generation.c
+++ map_generation.c
@@ -59,7 +59,7 @@ double noise(double x, double y, double z) {
 
 int generate_terrain (int size, float z_layer, float **z) {
     // Scaling Factors 
-    float scaling[] = {0.01, 0.15, 0.25, 0.5, 1, 1.5, 2, 2.5};
+    float scaling[] = {0.25, 0.5, 1, 1.5, 2, 2.5, 3, 3.5};
     int octaves = 8;
 
     // Fill array
