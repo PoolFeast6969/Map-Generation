8f6588c898b4c5077e8cf742e394c0c59a0d4397
Conman2 <richardharris443@gmail.com>
2018-11-19 18:17:14 +1100

    Beggining of the 3d era

--- render.c
+++ render.c
@@ -1,374 +1,103 @@
-/* lmao */
-#include <stdlib.h>
+///3d Rendering Engine 
+
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include <SDL2/SDL.h>
 
-#include "SDL2/SDL.h"
+// Screen Properties
+int screen_width = 1000;
+int screen_height = 500;
 
-// The compliler needs to know that this function exists before it calls it, or something like that
-int generate_terrain (int size, double x_layer, double y_layer, double z_layer, float **z);
+//Variable Devlerations
+float camera_pos[3] = {0, 0, 0};
+float camera_ang[3] = {0, 0, 0};
+float view_angle = 60; 
+int z_max_distance = 10;
+int z_min_distance = 1;
 
-struct terrain_layer {
-    int start_color[4];
-    int end_color[4];
-    float end_height;
-    float start_height;
-};
+//Function Declerations
+int render(x, y, z, phi, z_max, distance, screen_width, screen_height);
+int projection(float position, float* prjection_matrix, float* screen_position);
 
-typedef Uint32 pixel;
-
-int get_terrain_pixels(pixel *pixels, int pixel_amount, struct terrain_layer layer, float **height, SDL_PixelFormat *pixel_format) {
-    // Convert height map to pixel color map
-    for(int x=0; x < pixel_amount; x++) {
-        for(int y=0; y < pixel_amount; y++) {
-                if (layer.start_height <= height[x][y] && layer.end_height >= height[x][y]) { // the layer of interest
-                    float pixel_color[4]; 
-                    for(int color=0; color <= 3; color++) {
-                        // linearly interpolate between the two end colors // TODO add something so beaches aren't so discontinious
-                        pixel_color[color] = layer.start_color[color] + ((layer.start_color[color] - layer.end_color[color])*(height[x][y]-layer.start_height))/(layer.start_height - layer.end_height);
-                    }
-                    pixels[x*pixel_amount + y] = SDL_MapRGBA(pixel_format,pixel_color[0],pixel_color[1],pixel_color[2],pixel_color[3]);
-            }
-        }
-    }
-    return 0;
-}
-
-int set_shadow(SDL_Texture* shadow_texture, float lightness) {
-    // Create cloud shadows by blacking out the cloud texture
-    SDL_SetTextureColorMod(shadow_texture, lightness, lightness, lightness);
-    SDL_SetTextureAlphaMod(shadow_texture, lightness);
-    return 0;
-}
-
-int main() {
-    //
-    // Init
-    //
-
-    printf("Starting\n");
-
-    SDL_Init( SDL_INIT_VIDEO );
-    SDL_Window *window = SDL_CreateWindow("planes but with less detail",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,1000,1000,SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+//Main Function 
+int main(){
+    //Setting up SDL
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window*window  = SDL_CreateWindow("Hopefully this is 3d", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 1000, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+    SDL_Renderer*renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
     SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
 
-    // Set pixel format to make the textures in
-    Uint32 pixel_format_id = SDL_PIXELFORMAT_RGBA32; // should be able to do whatever you want here, just remember to change typedef pixel
-    SDL_PixelFormat *pixel_format = SDL_AllocFormat(pixel_format_id); // Get the actual format object from its ID
-    
-    //
-    // Terrain Heights Array
-    //
+    //Non-compiling Set-up
+    float aspect_ratio = screen_height/screen_width;
+    float z_normalize = z_max_distance/(z_max_distance - z_min_distance);
+    float feild_of_view = 1/tan(view_angle/2);
 
-    // yep all these pixels ae the same size
-    double pixel_scaling = 15;
+    float projection_matrix[4][4] = {
+        {aspect_ratio*feild_of_view, 0,             0,                           0},
+        {0,                          feild_of_view, 0,                           0},
+        {0,                          0,             z_normalize,                 1},
+        {0,                          0,             -z_max_distance*z_normalize, 0}
+    };
 
-    // based off window size
-    int terrain_size = 1000/pixel_scaling+1;
+    while(){
+        //Draw Sky
 
-    // Make array for the terrain generator to fill (a texture i guess)
-    // Allocating memory for the matrix which will store the altitudes
-    // Allocate the first dimension as an array of float pointers
-    float **height = malloc(sizeof(float*)*terrain_size);
-    // Allocate each float pointer as an array of actual floats
-    for (int i=0; i<terrain_size; i++) {
-        height[i] = malloc(sizeof(float)*terrain_size);
+        //Draw Land 
+        render();
     }
 
-    generate_terrain(terrain_size, 0, 0, 3.0, height); // Get a terrain height map
-
-    //
-    // Background
-    //
-
-    struct terrain_layer biome[] = {
-        {
-            .start_color = {36,36,85,255}, // Deep water
-            .end_color = {36,109,170,255}, // Shallow water
-            .start_height = 0, // Minimum value
-            .end_height = 0.4, // Minimum value
-        },{
-            .start_color = {148,148,123,255}, // low sand
-            .end_color = {170,180,150,255}, // high sand
-            .start_height = 0.4, // Minimum value
-            .end_height = 0.45, // halfway
-        },{
-            .start_color = {0,125,0,255}, // low land
-            .end_color = {0,218,0,255}, // high land
-            .start_height = 0.45, // Minimum value
-            .end_height = 1, // halfway
-        }
-    };
-    struct terrain_layer clouds = {
-        .start_color = {180,218,241,255}, // Deep water
-        .end_color = {255,255,255,255}, // Shallow water
-        .start_height = 0.5, // Minimum value
-        .end_height = 0.803, // Maximum value
-    };
-
-
-    pixel* land_pixels = malloc(sizeof(pixel)*terrain_size*terrain_size);
-    for(int layer=0; layer < sizeof(biome) / sizeof(struct terrain_layer); layer++) {
-        get_terrain_pixels(land_pixels, terrain_size, biome[layer], height, pixel_format);
-    }
-
-    // Put the land image into a texture
-    SDL_Surface *land_surface = SDL_CreateRGBSurfaceWithFormatFrom(land_pixels, terrain_size, terrain_size, 32,terrain_size * sizeof(pixel), pixel_format_id); // Through a surface 
-    
-    struct background_layer {
-        int distance; // Distance from view
-        double density; // Cloud thickness
-        int shadow_offset[2];
-        double position[2];
-        double last_update_time[2]; // When each position was last updated
-        SDL_Texture* texture; // Texture in video memory
-        pixel* pixels; // Pixel data in normal memory
-    };
-
-    // Instantiate the land layer struct
-    struct background_layer land = {
-        .distance = 500,
-        .texture = SDL_CreateTextureFromSurface(renderer,land_surface),
-        .pixels = land_pixels
-    };
-
-    SDL_FreeSurface(land_surface);
-
-    // Creates an array of cloud layers with their height and density already set, and the land already in the background
-    struct background_layer background_layers[] = {land,{400,300,}};
-    
-    int background_layer_amount = sizeof(background_layers) / sizeof(struct background_layer);
-    printf("There are %i background layer(s)\n",background_layer_amount);
-
-    // Convert height map to clouds
-    for (int i = 1; i < background_layer_amount; i++) {      
-        // Allocate pixel memory and return its pointer
-        background_layers[i].pixels = malloc(sizeof(pixel)*terrain_size*terrain_size);
-        // Set pixels transparent, could be done faster, but what if transparent isn't all zeros?
-        for(int p=0;p<terrain_size*terrain_size;p++) background_layers[i].pixels[p] = SDL_MapRGBA(pixel_format,0,0,0,0);
-        // Run terrain generation
-        generate_terrain(terrain_size, 0, 0, 100.0, height);
-        // Create a cloud pixel map from a height map
-        get_terrain_pixels(background_layers[i].pixels, terrain_size, clouds ,height, pixel_format);
-        SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormatFrom(background_layers[i].pixels, terrain_size, terrain_size, 0,terrain_size * sizeof(pixel), pixel_format_id);
-        // only for color formats without an alpha channel
-        SDL_SetColorKey(surface,SDL_TRUE,SDL_MapRGBA(pixel_format,0,0,0,0));
-        background_layers[i].texture = SDL_CreateTextureFromSurface(renderer,surface);
-    }
-    
-    //
-    // Sprites
-    //
-    const char *dir_name[] = {"Sprites"}; 
-    const char *sprite_names[] = {"Spitfire"};
-    int character_amount = 1;
-    int sprite_amount = 11;
-    int sprite_middle = (sprite_amount - 1)/2; 
-    
-    struct sprite {
-        SDL_Surface* surface;
-        SDL_Texture* texture;
-        int size;
-        int render; // 1 to render and 0 to not. 
-        int number; 
-        char filename[];
-    };
-
-    struct character {
-        double position[2];
-        double velocity[2];
-        double last_update_time[2];
-    };
-    
-    struct sprite sprites[sprite_amount];
-    struct character character[character_amount];
-
-    // Loaintd sprites
-    for (int j = 0; j < character_amount; j++){   
-        character[j].position[1] = 50;
-        character[j].position[0] = 50;
-        character[j].velocity[0] = 0;
-        character[j].velocity[1] = 0;    
-        for (int i = 0; i < sprite_amount; i++) {
-            char spt[80];
-            sprintf(spt, "%i", i+1);
-            // Organise the filename extension
-            strcpy(sprites[i].filename, dir_name[j]);
-            strcat(sprites[i].filename, "/");
-            strcat(sprites[i].filename, sprite_names[j]);
-            strcat(sprites[i].filename, spt);
-            strcat(sprites[i].filename, ".bmp");
-            puts(sprites[i].filename);
-            // Load the sprite from the image
-            sprites[i].surface = SDL_LoadBMP(sprites[i].filename);
-            // Convert to the pixel format we're using
-            SDL_ConvertSurface(sprites[i].surface, pixel_format, 0);
-            // Create texture
-            sprites[i].texture = SDL_CreateTextureFromSurface(renderer, sprites[i].surface);
-            // Delete surface or something
-            sprites[i].number = i + 1; 
-
-            if (i == sprite_middle){
-                sprites[i].render = 1;
-            } else {
-                sprites[i].render = 0;
-            }
-        }
-    }
-
-    //
-    // Loop
-    //
-
-    double view_velocity[] = {0,0};
-    SDL_Event window_event;
-    int window_h;
-    int window_w;
-
-    bool run = true;
-    int velocity = 1;
-    int left_speed = 0; 
-    int right_speed = 0; 
-    int up_speed = 0; 
-    int down_speed = 0;
-    int animation_speed = 50;
-    float animation_time = SDL_GetTicks();   
-
-    // Main loop that updates at vsync in case we ever need animations
-    while (run) {
-        while (SDL_PollEvent(&window_event)){
-            switch( window_event.type ){
-                case SDL_QUIT:
-                    run = false;
-                    break;
-                    // Look for a keypress
-                case SDL_KEYDOWN:
-                    // Check the SDLKey values and move change the coords
-                    switch( window_event.key.keysym.sym ){
-                        case SDLK_LEFT:
-                            left_speed = velocity;
-                            break; 
-                        case SDLK_RIGHT:
-                            right_speed = velocity;                      
-                            break; 
-                        case SDLK_UP:
-                            up_speed = velocity;
-                            break; 
-                        case SDLK_DOWN:
-                            down_speed = velocity;
-                            break; 
-                        default:    
-                            break;
-                    }
-                    break;
-
-                case SDL_KEYUP:
-                    // Check the SDLKey values and move change the coords
-                    switch( window_event.key.keysym.sym ){
-                        case SDLK_LEFT:
-                            left_speed = 0;                           
-                            break;
-                        case SDLK_RIGHT:
-                            right_speed = 0;                            
-                            break;
-                        case SDLK_UP:
-                            up_speed = 0;
-                            break;
-                        case SDLK_DOWN:
-                            down_speed = 0;
-                            break;
-                        default:    
-                            break;
-                    }
-                    break;
-            }
-        }
-
-        // Draw each of the background layers with the correct position
-        for (int i = 0; i < background_layer_amount; i++) {
-            // Determine if it needs to be have its position changed
-            for (int a = 0; a < 2 ; a++) {
-                double velocity = view_velocity[a]/background_layers[i].distance;
-                double time_since_update = SDL_GetTicks() - background_layers[i].last_update_time[a];
-                background_layers[i].position[a] = background_layers[i].position[a] + velocity * time_since_update;
-                background_layers[i].last_update_time[a] = SDL_GetTicks();
-            }
-
-            SDL_GetRendererOutputSize(renderer, &window_w, &window_h);
-            SDL_Rect new_position = {background_layers[i].position[0], background_layers[i].position[1], terrain_size*pixel_scaling, terrain_size*pixel_scaling};
-            // Add to frame
-            SDL_RenderCopy(renderer,background_layers[i].texture,NULL,&new_position);
-        }
-
-        // This records the velocity and postion for the character         
-        for (int j = 0; j < character_amount; j++) {
-            //Setting the Sprites Velocity 
-            character[j].velocity[0] = right_speed - left_speed;
-            character[j].velocity[1] = down_speed - up_speed; 
-
-            //Stop you from getting speeding tickets on diagnols 
-            if (character[j].velocity[0] != 0 && character[j].velocity[1] != 0){
-                if ((character[j].velocity[0] == velocity || character[0].velocity[0] == -velocity ) && (character[j].velocity[1] == velocity || character[j].velocity[1] == -velocity)){
-                    character[j].velocity[0] = character[j].velocity[0]*0.707; 
-                    character[j].velocity[1] = character[j].velocity[1]*0.707;
-                }   
-            } 
-
-            // Update the Position     
-            for (int a = 0; a < 2 ; a++) {    
-                double time_since_update = SDL_GetTicks() - character[j].last_update_time[a];
-                character[j].position[a] = character[j].position[a] + character[j].velocity[a] * time_since_update;
-                character[j].last_update_time[a] = SDL_GetTicks();
-            }  
-
-            //This manages the sprites for animations 
-            for (int i = 0; i < sprite_amount; i++) {     
-                //Roll animation command
-                if (sprites[i].render == 1 && (SDL_GetTicks() - animation_time > animation_speed)){
-                    //Rolling when moving left/right 
-                    if ((character[j].velocity[0] < 0) && (sprites[i].number > 1)){
-                        sprites[i - 1].render = 1; 
-                        sprites[i].render = 0; 
-                        animation_time = SDL_GetTicks(); 
-                    } else if ((character[j].velocity[0] > 0) && (sprites[i].number < 11)){
-                        sprites[i + 1].render = 1; 
-                        sprites[i].render = 0;    
-                        animation_time = SDL_GetTicks(); 
-
-                    //Returning to middle when not moving                                       
-                    } else if ((character[j].velocity[0] == 0) && (sprites[i].number > sprite_middle + 1)){
-                        sprites[i - 1].render = 1; 
-                        sprites[i].render = 0; 
-                        animation_time = SDL_GetTicks(); 
-                    } else if ((character[j].velocity[0] == 0) && (sprites[i].number < sprite_middle + 1)){
-                        sprites[i + 1].render = 1; 
-                        sprites[i].render = 0; 
-                        animation_time = SDL_GetTicks();     
-                    }   
-                }      
-
-                // Render the sprites
-                if (sprites[i].render == 1){
-                    SDL_Rect dest = {character[j].position[0],character[j].position[1],sprites[i].surface->w*pixel_scaling,sprites[i].surface->h*pixel_scaling};
-                    SDL_RenderCopyEx(renderer, sprites[i].texture, NULL, &dest, 0, NULL, SDL_FLIP_NONE);
-                }
-            }
-        }    
-        
-        SDL_RenderPresent(renderer); // Show the completed frame and wait for vsync
-        SDL_RenderClear(renderer); // Erase the screen (first action of the new frame)
-    }
-
-    //
-    // Clean Up
-    //
-    free(height);    
-    // Destroy things
+    //Destroy and Clode SDL after program finishes
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    //Quit SDL
+
     SDL_Quit();
     return 0;
 }
+
+//Render Function, Responable for finding what pixels on the screen need to be drawn and drawing them
+int render(float **height , float phi, float z_max, float distance, int screen_width, int screen_height, float projection_matrix){
+    //Clear Current Render 
+    SDL_RenderClear(renderer);
+
+    //Setting up the Y-buffer for each coloumn 
+
+    //For each pixel 
+    for(int i = 0; i < distance; i++){
+        for(int j = 0; i < screen_width; i++){
+            float position[3] = {i, j, height[i][j]};
+            
+            //Finding the screen co-ordinate
+            float *screen_position[3];
+            projection(position, projection_matrix, screen_position);
+
+            //Draw Line 
+            SDL_SetRenderDrawColor(renderer , 0, 0, 0); 
+            SDL_RenderDrawLine(renderer, j, screen_position[1], j, y_buffer[i]);
+
+            //Update Y_buffer if larger
+            if (screen_position[1] < y_buffer[i]){
+                y_buffer[i] = screen_position[i];
+            }
+        }
+    }
+}
+
+//Turns coordinates from 3d coordinate system into the Screens 2d cooridinate system
+float projection(float position, float* prjection_matrix, float* screen_position){
+    //Mulitplying the input co-ordinate by the projection matrix to transform it into 3d (COULD BE A FOR LOOP)
+    *screen_position[0] = position[0]*prjection_matrix[0][0] + position[1]*prjection_matrix[1][0] + position[2]*prjection_matrix[2][0] + prjection_matrix[3][0];
+    *screen_position[1] = position[0]*prjection_matrix[0][1] + position[1]*prjection_matrix[1][1] + position[2]*prjection_matrix[2][1] + prjection_matrix[3][1];
+    *screen_position[2] = position[0]*prjection_matrix[0][2] + position[1]*prjection_matrix[1][2] + position[2]*prjection_matrix[2][2] + prjection_matrix[3][2];
+    float scale         = position[0]*prjection_matrix[0][3] + position[1]*prjection_matrix[1][3] + position[2]*prjection_matrix[2][3] + prjection_matrix[3][3]; 
+
+    if (scaler != 0){
+        *screen_position[0] /= scale;
+        *screen_position[1] /= scale;
+        *screen_position[2] /= scale;
+    }
+}
+
+
+
